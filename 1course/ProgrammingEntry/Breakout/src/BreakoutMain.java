479456a0c18d0ec3cb425d505e3f13159baa512a
maxlshk <max.loshak.i@gmail.com>
2022-12-03 23:37:16 +0200

    first Breakout commit

--- 1course/ProgrammingEntry/Breakout/src/BreakoutMain.java
+++ 1course/ProgrammingEntry/Breakout/src/BreakoutMain.java
@@ -1,12 +1,9 @@
-/**
- * 
- */
 
 /**
- * @author Max Loshak @author Vladyslava Rudas
+ * @author Maksym Loshak @author Vladyslava Rudas
  * 
  * @version 30.11.2022
- * 
+ * no recent changes 
  */
 /*
  * File: Breakout.java
@@ -26,6 +23,8 @@ import java.awt.event.*;
 
 public class BreakoutMain extends GraphicsProgram {
 
+
+	private static final long serialVersionUID = 1L;
 /** Width and height of application window in pixels */
 	public static final int APPLICATION_WIDTH = 400;
 	public static final int APPLICATION_HEIGHT = 600;
@@ -65,13 +64,126 @@ public class BreakoutMain extends GraphicsProgram {
 
 /** Number of turns */
 	private static final int NTURNS = 3;
+	
+/** The item of GObject paddle (GRect) */
+	private static GRect paddle;
+	
+/** The item of GObject ball (GOval) */
+	private static GOval ball;
+	
+/** Random generating algorithm */
+	private static RandomGenerator rgen = RandomGenerator.getInstance();
+	
+/** Static color of the paddle */
+	private static final Color paddleColor = new Color(rgen.nextInt(1, 255), rgen.nextInt(1, 255), rgen.nextInt(1, 255));
 
 /* Method: run() */
-/** Runs the Breakout program. */
+/** Runs the BreakoutMain program. */
 	public void run() 
 	{
-		/* You fill this in, along with any subsidiary methods */
-
+		setup();
+		addMouseListeners();
 	}
-
+/**
+ * Sets the size of the console
+ * Sets all necessary variables	values
+ * Adds all necessary in-game items 
+ * @author Maksym Loshak
+ */
+	public void setup()
+	{
+		this.setSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
+		bricks();
+		paddle=getPaddle();
+		ball=getBall();
+		add(paddle, (WIDTH-PADDLE_WIDTH)/2, HEIGHT-PADDLE_Y_OFFSET-PADDLE_HEIGHT);
+		add(ball, (WIDTH-BALL_RADIUS)/2, (HEIGHT-BALL_RADIUS)/2);
+	}
+	
+	//*************************************************************************************
+	
+	
+	
+/**
+ * Creates bricks 10x10 (rows x columns) size set in the header.
+ * @author Maksym Loshak
+ */
+	private void bricks()
+	{
+		for(int i=0; i<NBRICK_ROWS; i++)
+		{
+			for(int u=0; u<NBRICKS_PER_ROW; u++)
+			{
+				GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
+				//int random = rgen.nextInt(1, 255);
+				//int random2 = rgen.nextInt(1, 255);
+				//int random3 = rgen.nextInt(1, 255);
+				Color brickColor = new Color(255-25*(i+u)/2, 25*(Math.abs((i-u))), 25*(i+u)/2);
+				brick.setFilled(true);
+				brick.setFillColor(brickColor);
+				add(brick, u*(BRICK_WIDTH+BRICK_SEP)+BRICK_SEP/2,BRICK_Y_OFFSET+i*(BRICK_HEIGHT+BRICK_SEP));
+			}
+		}
+		
+	}
+/**
+ * Creates an Object of GObject class (GRect)
+ * in-game paddle
+ * @author Maksym Loshak
+ * @return paddle
+ */
+	private GRect getPaddle()
+	{
+		GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle.setFilled(true);
+		paddle.setFillColor(paddleColor);
+		return paddle;
+	}
+/**
+ * Creates an Object of GObject class (GOval)
+ * in-game ball
+ * @author Maksym Loshak
+ * @return ball
+ */
+	private GOval getBall()
+	{
+		GOval ball = new GOval(BALL_RADIUS, BALL_RADIUS);
+		ball.setFilled(true);
+		ball.setFillColor(Color.cyan);
+		return ball;
+	}
+	
+	
+	
+	
+	//***************************************************************************************
+	
+	
+	
+/**
+ * Validates movement of the paddle in the in-game area due to the movement of the cursor.
+ * @author Maksym Loshak
+ */
+	public void mouseMoved(MouseEvent e)
+	{
+		remove(paddle);
+		paddle=null;
+		paddle = getPaddle();
+		if(e.getX()<=WIDTH-PADDLE_WIDTH/2&&e.getX()>=PADDLE_WIDTH/2)
+			add(paddle, e.getX()-getPaddle().getWidth()/2, HEIGHT-PADDLE_Y_OFFSET-PADDLE_HEIGHT);
+		else if(e.getX()>WIDTH-PADDLE_WIDTH/2)
+			add(paddle, WIDTH-PADDLE_WIDTH, HEIGHT-PADDLE_Y_OFFSET-PADDLE_HEIGHT);
+		else if(e.getX()<PADDLE_WIDTH/2)
+			add(paddle, 0, HEIGHT-PADDLE_Y_OFFSET-PADDLE_HEIGHT);
+	}
+	
+	private void ballMovement()
+	{
+		
+	}
+	
+	
+	
+	
+	
 }
